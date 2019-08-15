04c1771580286a13813eb09fc29ce7be7bb1c3bb
gzydong <r.yuandong@qq.com>
2019-08-15 22:19:36 +0800

    first commit

---
+++ src/Exceptions/Exception.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace Gzydong\Weather\Exceptions;
+
+
+class Exception extends \Exception
+{
+
+}
---
+++ src/Exceptions/HttpException.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace Gzydong\Weather\Exceptions;
+
+
+class HttpException extends Exception
+{
+
+}
