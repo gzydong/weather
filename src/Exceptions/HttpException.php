29a00c533c3871a821b55c898570dfb0fe26bbfa
gzydong <r.yuandong@qq.com>
2019-08-15 15:22:43 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- src/Exceptions/HttpException.php
+++ src/Exceptions/HttpException.php
@@ -1,10 +1,16 @@
 <?php
 
+/*
+ * This file is part of the gzydong/weather.
+ *
+ * (c) overtrue <r.yuandong@qq.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
 
 namespace Gzydong\Weather\Exceptions;
 
-
 class HttpException extends Exception
 {
-
 }
