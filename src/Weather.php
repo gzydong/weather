fe19ec1ab7eb505379bf03fbd21375d7da44afa8
gzydong <r.yuandong@qq.com>
2019-08-15 22:33:57 +0800

    添加方法

--- src/Weather.php
+++ src/Weather.php
@@ -62,4 +62,15 @@ class Weather
 
         return 'json' === $format ? json_decode($response, true) : $response;
     }
+
+
+    public function getLiveWeather($city, $format = 'json')
+    {
+        return $this->getWeather($city, 'base', $format);
+    }
+
+    public function getForecastsWeather($city, $format = 'json')
+    {
+        return $this->getWeather($city, 'all', $format);
+    }
 }
